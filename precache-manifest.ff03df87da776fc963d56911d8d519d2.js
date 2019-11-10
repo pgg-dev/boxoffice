74f9ff52b917684ce6d5fde773b85c9c0cc1b97a
pgg-dev <park.geumgyu@gmail.com>
2019-11-10 18:52:22 +0900

    Updates

---
+++ precache-manifest.ff03df87da776fc963d56911d8d519d2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8a342aa55b9881a5502e64a5a9c199d7",
+    "url": "/boxoffice/index.html"
+  },
+  {
+    "revision": "882f77bf8d2012a4bf71",
+    "url": "/boxoffice/static/css/main.e5d72f40.chunk.css"
+  },
+  {
+    "revision": "57f56557d3b17b0ce702",
+    "url": "/boxoffice/static/js/2.651c45c7.chunk.js"
+  },
+  {
+    "revision": "882f77bf8d2012a4bf71",
+    "url": "/boxoffice/static/js/main.d21704a0.chunk.js"
+  },
+  {
+    "revision": "9f22102af22889fdaf4d",
+    "url": "/boxoffice/static/js/runtime-main.0652679e.js"
+  }
+]);
